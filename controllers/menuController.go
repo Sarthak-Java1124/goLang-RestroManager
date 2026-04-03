98a9bb8bb8d8e5bad07d69eab431125baff0f4d9
Sarthak-Java1124 <sarthak@fensory.com>
2026-04-03 22:17:41 +0530

    feat : added the get menu code for the menu controller

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -9,27 +10,47 @@ import (
 	"github.com/Sarthak-Java1124/goLang-RestroManager.git/database"
 	"github.com/Sarthak-Java1124/goLang-RestroManager.git/models"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 var menuCollection = database.OpenCollection(*database.DBinstance(), "menu")
 
 func GetMenus() gin.HandlerFunc {
 	return func(c *gin.Context) {
-     page := c.Query("page")
-	 limit := c.Query("limit")
-     
-	 matchStage := bson.D{{Key: "$match" , Value: bson.D{}}}
-	facetStage := bson.D{{Key: "$facet", Value: bson.D{
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		page := c.Query("page")
+
+		limit := c.Query("limit")
+
+		matchStage := bson.D{{Key: "$match", Value: bson.D{}}}
+		facetStage := bson.D{{Key: "$facet", Value: bson.D{
 			{Key: "metadata", Value: bson.D{{Key: "$count", Value: "total_count"}}},
 			{Key: "data", Value: bson.A{
-				bson.D{{Key: "$skip", Value: startIndex}},
-				bson.D{{Key: "$limit", Value: endIndex}},
+				bson.D{{Key: "$skip", Value: page}},
+				bson.D{{Key: "$limit", Value: limit}},
 			}},
 		}}}
-	projectStage := bson.D{{Key:  "$project" , Value: bson.D{{Key : "metadata"  , Value : bson.D{{Key : "$count" , Value:  "total_count"}}} , {Key: "data" , Value : "$data"}}}}
-}
+		projectStage := bson.D{{Key: "$project", Value: bson.D{{Key: "metadata", Value: bson.D{{Key: "$count", Value: "total_count"}}}, {Key: "data", Value: "$data"}}}}
+		result, err := menuCollection.Aggregate(ctx, mongo.Pipeline{matchStage,
+			facetStage,
+			projectStage})
+		if err != nil {
+			log.Fatal("The error in menu aggregation is :", err)
 
+		}
+		var final_result bson.M
+
+		if err = result.All(ctx, &final_result); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, final_result)
+
+	}
+}
 func GetMenu() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		id := c.Param("menu_id")
@@ -47,7 +68,26 @@ func GetMenu() gin.HandlerFunc {
 
 func CreateMenu() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var menuBody models.Menu
+		var foodBody models.FoodModel
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		if err := c.BindJSON(&menuBody); err != nil {
+			log.Fatal("The error in binding the request json in the menu controller is : ", err)
+		}
+		validationErr := validate.Struct(&menuBody)
+		if validationErr != nil {
+			fmt.Println("The validation error is : ", validationErr)
+		}
+		err := foodCollection.FindOne(ctx, bson.M{"food_id": menuBody.Food_id}).Decode(&foodBody)
+		if err != nil {
+			log.Fatal("The food id is invalid for the given menu")
+		}
 
+		menuBody.ID = primitive.NewObjectID()
+		now := time.Now()
+		menuBody.Created_at = now
+		menuBody.Updated_at = now
 	}
 }
 
