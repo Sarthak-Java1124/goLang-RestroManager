d238726b50f4395a263d12599567c420f7ff64c3
Sarthak-Java1124 <sarthak@fensory.com>
2026-04-07 19:30:47 +0530

    feat : added jwt token gemerator

---
+++ utils/tokenGenerator.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"crypto/rsa"
+	"log"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type JWTPayload struct {
+	userId primitive.ObjectID `json:"_id"`
+	Email  string
+	jwt.RegisteredClaims
+}
+
+func generateJWTToken(userId primitive.ObjectID, email string, privateKey *rsa.PrivateKey) string {
+
+	claims := JWTPayload{
+		Email:  email,
+		userId: userId,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   userId.Hex(),
+			Issuer:    "api-service",
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(10 * time.Minute)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+	tokens := jwt.NewWithClaims(jwt.SigningMethodEdDSA, claims)
+	signedKey, err := tokens.SignedString(privateKey)
+	if err != nil {
+		log.Fatal("There was an error signing jwt strings")
+		return ""
+	}
+	return signedKey
+
+}
